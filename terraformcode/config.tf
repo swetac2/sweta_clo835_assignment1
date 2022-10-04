bf987c83e6a90c8fa8941591702441d1f6648fa0
swetac2 <103720160+swetac2@users.noreply.github.com>
2022-10-03 23:24:43 -0400

    Deleted terraform code

--- terraformcode/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "assignment1-sweta"             // Bucket where to SAVE Terraform State
-    key    = "assignment1/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                     // Region where bucket is created
-  }
-}
